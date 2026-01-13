b076874a525c72eee35fa8bb907c7a35d9e62a03
Jordy Langdon <langdonjordy01@gmail.com>
2026-01-13 23:40:55 +0000

    File changes

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -280,7 +280,7 @@ export default function Library() {
                 </p>
               </div>
             ) : (
-              <div className="grid grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {filteredTemplates.map(template => (
                   <TemplateCard 
                     key={template.id} 
