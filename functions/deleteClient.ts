a5d1e3ee2b7d630880287005042ed35028115f04
Jordy Langdon <langdonjordy01@gmail.com>
2026-01-15 23:27:22 +0000

    File changes

--- functions/deleteClient.ts
+++ functions/deleteClient.ts
@@ -24,42 +24,95 @@ Deno.serve(async (req) => {
     // Delete all workflow instances related to this client
     const workflows = await base44.entities.WorkflowInstance.filter({ client_id });
     for (const workflow of workflows) {
-      // Delete stages, deliverables, tasks, and documents
       const stages = await base44.entities.StageInstance.filter({ workflow_instance_id: workflow.id });
       for (const stage of stages) {
         const deliverables = await base44.entities.DeliverableInstance.filter({ stage_instance_id: stage.id });
         for (const deliverable of deliverables) {
-          const tasks = await base44.entities.TaskInstance.filter({ deliverable_instance_id: deliverable.id });
-          for (const task of tasks) {
-            await base44.asServiceRole.entities.TaskInstance.delete(task.id);
+          try {
+            const tasks = await base44.entities.TaskInstance.filter({ deliverable_instance_id: deliverable.id });
+            for (const task of tasks) {
+              try {
+                await base44.asServiceRole.entities.TaskInstance.delete(task.id);
+              } catch (e) {
+                console.log('Task already deleted:', task.id);
+              }
+            }
+          } catch (e) {
+            console.log('Tasks cleanup error:', e.message);
           }
-          const documents = await base44.entities.DocumentInstance.filter({ deliverable_instance_id: deliverable.id });
-          for (const doc of documents) {
-            await base44.asServiceRole.entities.DocumentInstance.delete(doc.id);
+          
+          try {
+            const documents = await base44.entities.DocumentInstance.filter({ deliverable_instance_id: deliverable.id });
+            for (const doc of documents) {
+              try {
+                await base44.asServiceRole.entities.DocumentInstance.delete(doc.id);
+              } catch (e) {
+                console.log('Document already deleted:', doc.id);
+              }
+            }
+          } catch (e) {
+            console.log('Documents cleanup error:', e.message);
           }
-          await base44.asServiceRole.entities.DeliverableInstance.delete(deliverable.id);
+          
+          try {
+            await base44.asServiceRole.entities.DeliverableInstance.delete(deliverable.id);
+          } catch (e) {
+            console.log('Deliverable already deleted:', deliverable.id);
+          }
         }
-        await base44.asServiceRole.entities.StageInstance.delete(stage.id);
+        try {
+          await base44.asServiceRole.entities.StageInstance.delete(stage.id);
+        } catch (e) {
+          console.log('Stage already deleted:', stage.id);
+        }
       }
-      await base44.asServiceRole.entities.WorkflowInstance.delete(workflow.id);
+      try {
+        await base44.asServiceRole.entities.WorkflowInstance.delete(workflow.id);
+      } catch (e) {
+        console.log('Workflow already deleted:', workflow.id);
+      }
     }
 
     // Delete communications
-    const communications = await base44.entities.CommunicationLog.filter({ source_entity_id: client_id });
-    for (const comm of communications) {
-      await base44.asServiceRole.entities.CommunicationLog.delete(comm.id);
+    try {
+      const communications = await base44.entities.CommunicationLog.filter({ source_entity_id: client_id });
+      for (const comm of communications) {
+        try {
+          await base44.asServiceRole.entities.CommunicationLog.delete(comm.id);
+        } catch (e) {
+          console.log('Communication already deleted:', comm.id);
+        }
+      }
+    } catch (e) {
+      console.log('Communications cleanup error:', e.message);
     }
 
     // Delete client contacts
-    const clientContacts = await base44.entities.ClientContact.filter({ client_id });
-    for (const cc of clientContacts) {
-      await base44.asServiceRole.entities.ClientContact.delete(cc.id);
+    try {
+      const clientContacts = await base44.entities.ClientContact.filter({ client_id });
+      for (const cc of clientContacts) {
+        try {
+          await base44.asServiceRole.entities.ClientContact.delete(cc.id);
+        } catch (e) {
+          console.log('ClientContact already deleted:', cc.id);
+        }
+      }
+    } catch (e) {
+      console.log('ClientContacts cleanup error:', e.message);
     }
 
     // Delete events related to client
-    const events = await base44.entities.Event.filter({ source_entity_id: client_id });
-    for (const event of events) {
-      await base44.asServiceRole.entities.Event.delete(event.id);
+    try {
+      const events = await base44.entities.Event.filter({ source_entity_id: client_id });
+      for (const event of events) {
+        try {
+          await base44.asServiceRole.entities.Event.delete(event.id);
+        } catch (e) {
+          console.log('Event already deleted:', event.id);
+        }
+      }
+    } catch (e) {
+      console.log('Events cleanup error:', e.message);
     }
 
     // Delete client
